c9540047355a7ab63496e07ee35bd434c797868c
Kiam9000 <maikj3@hotmail.com>
2015-01-03 14:19:42 +0100

    Maik

--- Challenge/Profile/Newsadmin.aspx.cs
+++ Challenge/Profile/Newsadmin.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Challenge.Profile
 {
-    public partial class Useradmin : System.Web.UI.Page
+    public partial class Newsadmin : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
